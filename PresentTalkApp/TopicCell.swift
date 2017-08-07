0c6751045d3ceb7cc112c3d828782b9b93977abd
PingAnXL <PingAnXL@PingAnXLdeMacBook-Pro.local>
2017-08-07 18:01:15 +0800

    Topic

---
+++ PresentTalkApp/TopicCell.swift
@@ -0,0 +1,18 @@
+//
+//  TopicCell.swift
+//  PresentTalkApp
+//
+//  Created by PingAnXL on 2017/8/7.
+//  Copyright © 2017年 PingAnXL. All rights reserved.
+//
+
+import UIKit
+
+class TopicCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
