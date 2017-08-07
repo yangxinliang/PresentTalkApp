dc55e1097ed8f1f64046f01d0c5e445f7309118c
PingAnXL <PingAnXL@PingAnXLdeMacBook-Pro.local>
2017-08-07 17:45:09 +0800

    热点和其他类型的控制器

---
+++ PresentTalkApp/BannerCell.swift
@@ -0,0 +1,18 @@
+//
+//  BannerCell.swift
+//  PresentTalkApp
+//
+//  Created by PingAnXL on 2017/8/7.
+//  Copyright © 2017年 PingAnXL. All rights reserved.
+//
+
+import UIKit
+
+class BannerCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ PresentTalkApp/BannerCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  BannerCollectionView.swift
+//  PresentTalkApp
+//
+//  Created by PingAnXL on 2017/8/7.
+//  Copyright © 2017年 PingAnXL. All rights reserved.
+//
+
+import UIKit
+
+class BannerCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
